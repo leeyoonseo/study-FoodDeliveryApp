e60e2847edb5a89e48ed5c314493da462facfb49
leeyoonseo <okayoon.lee@gmail.com>
2022-03-31 17:28:06 +0900

    리액트 내비게이션 - 로그인화면 만들기까지

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,12 +1,99 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useCallback, useState} from 'react';
+import {
+  Alert,
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  StyleSheet,
+} from 'react-native';
 
 function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const canGoNext = email && password;
+
+  const onChangeEmail = useCallback(text => {
+    setEmail(text);
+  }, []);
+
+  const onChangePassword = useCallback(text => {
+    setPassword(text);
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    Alert.alert('알림', '안녕~');
+  }, []);
+
   return (
     <View>
-      <Text>로그인</Text>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>이메일</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="이메일을 입력해주세요."
+          onChangeText={onChangeEmail}
+        />
+      </View>
+      <View style={styles.inputWrapper}>
+        <Text style={styles.label}>비밀번호</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="비밀번호를 입력해주세요."
+          onChangeText={onChangePassword}
+        />
+      </View>
+      <View style={styles.buttoZone}>
+        <Pressable
+          style={
+            !canGoNext
+              ? styles.loginButton
+              : // : [styles.loginButton, styles.loginButtonActive]
+                StyleSheet.compose(styles.loginButton, styles.loginButtonActive)
+          }
+          onPress={onSubmit}
+          disabled={!canGoNext}>
+          <Text style={styles.loginButtonText}>로그인</Text>
+        </Pressable>
+        <Pressable style={styles.loginButton} onPress={onSubmit}>
+          <Text style={styles.loginButtonText}>회원가입</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputWrapper: {
+    padding: 20,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  textInput: {
+    padding: 5,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  buttoZone: {
+    alignItems: 'center',
+  },
+  loginButton: {
+    backgroundColor: 'gray',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  loginButtonActive: {
+    backgroundColor: 'blue',
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
 
 export default SignIn;
