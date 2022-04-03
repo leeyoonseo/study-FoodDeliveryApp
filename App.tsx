8754d590334dd7bbf7fecc6e1fa199f0a8a6111a
leeyoonseo <okayoon.lee@gmail.com>
2022-04-03 17:52:56 +0900

    서버 API와 연동하기 완료

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
-import AppInner from './AppInner';
 import store from './src/store';
+import AppInner from './AppInner';
 
 function App() {
   return (
     <Provider store={store}>
-      <AppInner />
+      <NavigationContainer>
+        <AppInner />
+      </NavigationContainer>
     </Provider>
   );
 }
